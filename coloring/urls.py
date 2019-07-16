61b5197fe96d6d7b0904759af5798fe65509f139
annezitafoo <annezitafoo@berkeley.edu>
2019-07-16 02:36:13 -0400

    <Completed>

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('touch', views.touch, name='touch')
+    path('', views.index, name='index')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
-  
-def touch(request):
-  return render(request, 'coloring/touch.html')
